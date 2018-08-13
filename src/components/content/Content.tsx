3abc90a19a03f8c43fe8b737c6f83328cfa14e5f
Luke Kozak <kozakluke@gmail.com>
2018-08-13 17:59:58 +0200

    Add alias to lib

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as style from './Content.pcss';
-import ResponsiveImage from '../src/index';
+import ResponsiveImage from 'lib/react-image/src';
 import {PureComponent} from 'react';
 import {Images} from './types';
 /**
